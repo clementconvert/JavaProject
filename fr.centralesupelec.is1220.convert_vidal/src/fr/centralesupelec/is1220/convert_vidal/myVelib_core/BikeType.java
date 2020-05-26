9f943d39beb30fee97fc6aed7173c9ab1d49ada6
clementconvert <clement.convert@student-cs.fr>
2020-05-26 17:12:03 +0200

    refresh environnement travail

--- fr.centralesupelec.is1220.convert_vidal/src/fr/centralesupelec/is1220/convert_vidal/myVelib_core/BikeType.java
+++
@@ -1,3 +0,0 @@
-package fr.centralesupelec.is1220.convert_vidal.myVelib_core;
-
-public enum BikeType {electrical, mechanical}
--- fr.centralesupelec.is1220.convert_vidal/src/fr/centralesupelec/is1220/convert_vidal/myVelib_core/CardType.java
+++
@@ -1,3 +0,0 @@
-package fr.centralesupelec.is1220.convert_vidal.myVelib_core;
-
-public enum CardType {Vlibre, Vmax}
